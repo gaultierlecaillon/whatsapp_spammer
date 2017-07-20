d757ccbf3cce09ff60fa8edabcaa0eb329a707d2
Gaultier Lecaillon <gaultier.lecail@gmail.com>
2017-07-20 11:48:14 +0200

    Remove jquery from the script

--- whatsapp_spam.js
+++ whatsapp_spam.js
@@ -1,18 +1,18 @@
-var script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(script);
-
-function whatsAppText(t) {
+function whatsAppText(message) {
 	window.InputEvent = window.Event || window.InputEvent;
 	var event = new InputEvent('input', {bubbles: true});
 	var textbox = document.querySelector('#main > footer > div.block-compose > div.input-container > div > div.input');
-	textbox.textContent = t;
+	textbox.textContent = message;
 	textbox.dispatchEvent(event);
-	$( "div[data-icon='send']" ).click();
+	document.querySelector('[data-icon="send"]').click()﻿;
 }
 
 function spam(message, loop) {
+	var delay = 1000; // 1 second
+	
 	for (var i = 0; i < loop; i++){
 		window.setTimeout(function(){
 			whatsAppText(message);
-		}, 1000*i);
+		}, delay*i);
 	}
 }
